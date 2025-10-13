ea8f4e88bf8b1dc61c74fbaaf8e23bd2c559e1ba
Cameron <cameronmills0713@gmail.com>
2025-10-13 17:04:02 -0400

    Started details page and added button to submit new tickets.

---
+++ TheBugTracker.Client/Helpers/MudBlazorExtensions.cs
@@ -0,0 +1,23 @@
+﻿using MudBlazor;
+using TheBugTracker.Client.Models.Enums;
+
+namespace TheBugTracker.Client.Helpers
+{
+    public static class MudBlazorExtensions
+    {
+        public static MudBlazor.Color GetColor(this ProjectPriority priority)
+        {
+
+            Color color = priority switch
+            {
+                ProjectPriority.Low => Color.Success,
+                ProjectPriority.Medium => Color.Secondary,
+                ProjectPriority.High => Color.Error,
+                ProjectPriority.Urgent => Color.Dark,
+                _ => Color.Default,
+
+            };
+            return color;
+        }
+    }
+}
